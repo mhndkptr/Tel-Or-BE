3080758ea3b25f355e6589594bed741eb7fb053f
mhndkptr <muhamadhendika77@gmail.com>
2025-05-10 01:20:30 +0700

    To Beyond & Infinity

---
+++ src/main/java/com/pbo/telor/TelorApplication.java
@@ -0,0 +1,13 @@
+package com.pbo.telor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbo/telor/TelorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbo.telor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
