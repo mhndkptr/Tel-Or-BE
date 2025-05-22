7ab51c7a4913e30766730d6c93e6f7fe9525235e
fazarirazkaa <fazari.razka50@gmail.com>
2025-05-22 16:46:04 +0700

    feat: add ormawa entity

---
+++ src/main/java/com/pbo/telor/model/OrmawaCommunityEntity.java
@@ -0,0 +1,18 @@
+package com.pbo.telor.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "ormawa_community")
+@Builder
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class OrmawaCommunityEntity extends OrmawaEntity {
+}
+
---
+++ src/main/java/com/pbo/telor/model/OrmawaEntity.java
@@ -0,0 +1,66 @@
+package com.pbo.telor.model;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ormawa")
+public abstract  class OrmawaEntity {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @NotBlank(message = "Full name is required")
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    @Column(nullable = false)
+    private String description;
+
+    @NotBlank(message = "Content is required")
+    @Column(nullable = false)
+    private String content;
+
+    @NotBlank(message = "IsOpenRegistration is required")
+    @Column(nullable = false, unique = true)
+    private boolean isOpenRegistration;
+
+    @NotBlank(message = "Icon is required")
+    @Column(nullable = false, unique = true)
+    private String icon;
+
+    @NotBlank(message = "Background is required")
+    @Column(nullable = false, unique = true)
+    private boolean background;
+
+    @NotBlank(message = "Category must be selected")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    public enum Category {
+        LABORATORY,
+        COMMUNITY,
+        ORGANIZATION,
+        UKM
+    }
+}
