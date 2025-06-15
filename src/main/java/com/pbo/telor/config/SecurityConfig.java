eb4edbf3615528eedc36c9cd81c424b6161a7c96
ardhiancalwa <ardhiancalwaofc@gmail.com>
2025-06-15 22:26:49 +0700

    chore(config): update configuration Open API and CORS

--- src/main/java/com/pbo/telor/config/SecurityConfig.java
+++ src/main/java/com/pbo/telor/config/SecurityConfig.java
@@ -64,7 +64,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // tanpa wildcard dan tanpa path
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://api.telor.muhhendikaputra.my.id",
+                "http://localhost:3636", "https://telor.muhhendikaputra.my.id"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Client-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
